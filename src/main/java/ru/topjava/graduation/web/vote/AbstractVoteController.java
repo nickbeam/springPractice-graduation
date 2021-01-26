ea6a611edffbcc70ab8686231cf18ed57c38b952
nick <dvininnick@gmail.com>
2021-01-26 08:22:01 +0300

    Refactoring AbstractVoteController

--- src/main/java/ru/topjava/graduation/web/vote/AbstractVoteController.java
+++ src/main/java/ru/topjava/graduation/web/vote/AbstractVoteController.java
@@ -1,6 +1,5 @@
 package ru.topjava.graduation.web.vote;
 
-import org.hibernate.validator.internal.metadata.aggregated.ValidatableParametersMetaData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +7,13 @@ import ru.topjava.graduation.model.Vote;
 import ru.topjava.graduation.service.RestaurantService;
 import ru.topjava.graduation.service.UserService;
 import ru.topjava.graduation.service.VoteService;
+import ru.topjava.graduation.util.ValidationUtil;
 
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-import static ru.topjava.graduation.util.ValidationUtil.assureIdConsistent;
-import static ru.topjava.graduation.util.ValidationUtil.checkNew;
 import static ru.topjava.graduation.web.SecurityUtil.authUserId;
 
 public abstract class AbstractVoteController {
@@ -29,25 +28,16 @@ public abstract class AbstractVoteController {
     @Autowired
     private RestaurantService restaurantService;
 
-    public Vote create(int restaurantId) {
-        log.info("create vote for restaurant with id = {}", restaurantId);
+    public void vote(int restaurantId) {
         int userId = authUserId();
-        if (!service.isVotedToday(userId)) {
-            Vote vote = new Vote(LocalDateTime.now(), userService.get(userId), restaurantService.get(restaurantId));
-            return service.create(vote, userId, restaurantId);
-        } else {
-            update(restaurantId);
-            return new Vote();
-        }
-    }
-
-    public void update(int restaurantId) {
-        int userId = authUserId();
+        log.info("user with id = {} vote for restaurant with id = {}", userId, restaurantId);
         List<Vote> votes = service.getUserVoteToday(userId);
-        Vote vote = votes.get(0);
-        log.info("update vote {} for restaurant with id = {}", vote, restaurantId);
-        assureIdConsistent(vote, vote.getId());
-        service.update(vote, userId, restaurantId);
+        if (votes.isEmpty()) {
+            Vote vote = new Vote(LocalDateTime.now(), userService.get(userId), restaurantService.get(restaurantId));
+            service.create(vote, userId, restaurantId);
+        } else {
+            service.update(votes.get(0), userId, restaurantId);
+        }
     }
 
     public void delete(int id, int restaurantId) {
@@ -56,10 +46,11 @@ public abstract class AbstractVoteController {
         service.delete(id, userId, restaurantId);
     }
 
-    public Vote get(int id, int restaurantId) {
-        log.info("get vote with id = {} for restaurant with id = {}", id, restaurantId);
+    public Vote getUserVote() {
         int userId = authUserId();
-        return service.get(id, userId, restaurantId);
+        log.info("get user with id = {} vote today", userId);
+        List<Vote> votes = service.getUserVoteToday(userId);
+        return ValidationUtil.checkNotFound(votes.get(0), "user vote for today not found");
     }
 
     public int getCount(int restaurantId) {
